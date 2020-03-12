44544345b1d18971d4e5f90f8014b87911ac87d8
Randy Steele <steelerx155@gmail.com>
2020-03-12 20:21:59 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,5 @@ Rails.application.routes.draw do
   #   end
     resources :coupons, only: [:index, :new, :create]
   get '/coupons/:id', to: 'coupons#show', as: 'coupon'
-  
+  post '/coupons/:id', to: 'coupons#show', as: 'coupon'
 end
