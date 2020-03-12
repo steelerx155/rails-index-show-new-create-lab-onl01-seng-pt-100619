30e0632c7d278235f2de53dd172528714f826ed0
Randy Steele <steelerx155@gmail.com>
2020-03-12 20:49:30 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,15 +1,15 @@
 class CouponsController < ApplicationController
   
-  def index
-      @coupons = Coupon.all
-    end
+  # def index
+  #     @coupons = Coupon.all
+  #   end
   
   def create 
     @coupons = Coupon.new
-    @coupons.coupon_code = params[:coupon_code]
-    @coupons.store = params[:store]
-    @coupons.save
-    redirect_to coupon_path(@coupons)
+    # @coupons.coupon_code = params[:coupon_code]
+    # @coupons.store = params[:store]
+    # @coupons.save
+    # redirect_to coupon_path(@coupons)
   end 
   
 end
